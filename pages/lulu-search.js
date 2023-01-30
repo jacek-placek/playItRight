4bb3fd3c5c83b548c0ad0cdda7b7be15928e6211
jacek-placek <incubus@o2.pl>
2023-01-30 11:01:39 +0100

    pr009

--- pages/lulu-search.js
+++ pages/lulu-search.js
@@ -7,16 +7,40 @@ exports.LuluSearchPage = class LuluSearchPage {
         this.page = page;
         this.searchInputField = page.getByTestId('header-search-input');
         this.searchResult = page.getByTestId('product-title');
-        //
+        this.noSearchResults = page.getByRole('heading');
+        
     }
 
     async goto() {
         await this.page.goto('https://www.lulu.com/search?page=1&q=&pageSize=10&adult_audience_rating=00'); //I'd try use env variable from config file (ie. 'portal_home_page')
     }
 
-    async searchInputField() {
-        await this.searchInputField.fill('');
+    async enterValueToSearchInputField(searchValue) {
+        await this.searchInputField.click();
+        await this.searchInputField.fill(searchValue);
+        await this.searchInputField.press('Enter');
     }
+
+    // async searchInputField() {
+    //     await this.searchInputField.click().fill('abc');
+    //     await this.searchInputField.press('Enter');
+    // }
+
+    async searchResultIsNotNull() {
+        var count = await this.searchResult.count();
+        expect(count).toBeGreaterThanOrEqual(1);
+    }
+
+    async searchResultIsNull() {
+        var count = await this.searchResult.count();
+        expect(count).toBeLessThan(1);
+    }
+
+    async noReasultsMessage() {
+        await this.noSearchResults().toHaveText('No search results')
+    }
+
+
 
 // search results is not null
     /*async searchResultIsNotNull() {
