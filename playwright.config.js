bf2a453f98c59a98fa910690549bbbab93f1a4ab
jacek-placek <incubus@o2.pl>
2023-04-03 14:19:53 +0200

    pr101

--- playwright.config.js
+++ playwright.config.js
@@ -1,11 +1,4 @@
-// @ts-check
 const { devices } = require('@playwright/test');
-
-/**
- * Read environment variables from file.
- * https://github.com/motdotla/dotenv
- */
-// require('dotenv').config();
 
 
 /**
@@ -60,50 +53,9 @@ const config = {
       },
     },
 
-    /*{
-      name: 'webkit',
-      use: {
-        ...devices['Desktop Safari'],
-      },
-    },*/
 
-    /* Test against mobile viewports. */
-    // {
-    //   name: 'Mobile Chrome',
-    //   use: {
-    //     ...devices['Pixel 5'],
-    //   },
-    // },
-    // {
-    //   name: 'Mobile Safari',
-    //   use: {
-    //     ...devices['iPhone 12'],
-    //   },
-    // },
+  ]
 
-    /* Test against branded browsers. */
-    // {
-    //   name: 'Microsoft Edge',
-    //   use: {
-    //     channel: 'msedge',
-    //   },
-    // },
-    // {
-    //   name: 'Google Chrome',
-    //   use: {
-    //     channel: 'chrome',
-    //   },
-    // },
-  ],
-
-  /* Folder for test artifacts such as screenshots, videos, traces, etc. */
-  // outputDir: 'test-results/',
-
-  /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   port: 3000,
-  // },
 };
 
 module.exports = config;
